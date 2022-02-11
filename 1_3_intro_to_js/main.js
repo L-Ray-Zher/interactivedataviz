7838a656f08e3068dfecf0f2657482aa1a8e1b45
rdaniell <rdaniell@pratt.edu>
2022-02-11 14:16:10 -0500

    added tables

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,12 @@
 console.log('hello world');
+
+const myName=("RJD")
+
+console.log(myName)
+
+const myStuff = ["thing1", "thing2", "thing3"]
+
+let mine = myStuff.map(d => d + " item")
+
+console.log(mine)
+
